a7b9cc0512b4dd08054f2a1fb8b5156b272f58ad
glacya <sunghyeck1779@gmail.com>
2022-07-13 08:30:28 +0000

    ?

--- server/main.js
+++ server/main.js
@@ -3,7 +3,7 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
 
-
+console.log("test?");
 
 server.listen(80, function() {
     console.log('Server is on!!');
