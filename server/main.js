38f9f0098c7aff6f39a2c90c3eeedeae61736a33
BLESS11186 <jinsook3248@naver.com>
2022-07-13 17:38:07 +0000

    add character move and request

--- server/main.js
+++ server/main.js
@@ -1,52 +1,27 @@
 var app = require('express')();
+var cors = require('cors');
+app.use(cors());
 var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var port = process.env.PORT || 80;
-
-var dir_name = "/root/forest_of_kaist/client/"
-
-// TEMP CODE: set rendering option to EJS
-app.set('view engine', 'ejs');
-
-app.get("/", (req, res) => {
-    // TODO: redirect to login page.
-    // TODO: if login cookie exists, redirect to main game page.
-    console.log("GET /");
-    // res.sendFile(dir_name + "test.html");
-    res.render(dir_name + "test.ejs");
+var io = require("socket.io")(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"]
+    }
 });
 
-app.get("")
+var port = process.env.PORT || 8000;
 
-// main game.
-// TODO: redirect to login page if not logged in.
-// TODO: deliver page to user.
-app.get("/game.js", (req, res) => {
-    console.log("hi");
-})
+app.get('/', function(req, res) {
+    res.send("got");
+ });
+ 
 
 io.on('connection', (socket) => {
-    
-    //socket.emit으로 현재 연결한 상대에게 신호를 보낼 수 있다.
-    socket.emit('usercount', io.engine.clientsCount);
-
-    // on 함수로 이벤트를 정의해 신호를 수신할 수 있다.
-    socket.on('message', (msg) => {
-        //msg에는 클라이언트에서 전송한 매개변수가 들어온다. 이러한 매개변수의 수에는 제한이 없다.
-        console.log('Message received: ' + msg);
-
-        // io.emit으로 연결된 모든 소켓들에 신호를 보낼 수 있다.
-        io.emit('message', msg);
-    });
-
-    socket.on('client_msg', (msg) => {
-        console.log("??");
-        io.emit('server_msg', "ㅋㅋㅋ");
-    });
+    socket.on("move", (msg) => console.log(`Got message from socket id ${socket.id}: (x: ${msg.x}, y: ${msg.y})`));
 });
 
 console.log("test?");
 
 server.listen(port, function() {
-    console.log('Server is on!!');
+    console.log('Server is on ' + port);
 });
